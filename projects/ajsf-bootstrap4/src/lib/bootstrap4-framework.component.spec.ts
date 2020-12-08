d50ee3e130abd6d6eb640c4a2fff6d66321f8c33
ethanbrooks <ethan.brooks@gmail.com>
2020-12-08 13:48:30 -0500

    Initial commit

---
+++ projects/ajsf-bootstrap4/src/lib/bootstrap4-framework.component.spec.ts
@@ -0,0 +1,37 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import {
+  JsonSchemaFormModule,
+  WidgetLibraryModule
+} from '@ajsf/core';
+import { Bootstrap4FrameworkComponent } from './bootstrap4-framework.component';
+
+describe('FwBootstrap4Component', () => {
+  let component: Bootstrap4FrameworkComponent;
+  let fixture: ComponentFixture<Bootstrap4FrameworkComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        JsonSchemaFormModule,
+        CommonModule,
+        WidgetLibraryModule,
+      ],
+      declarations: [Bootstrap4FrameworkComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Bootstrap4FrameworkComponent);
+    component = fixture.componentInstance;
+    component.layoutNode = { options: {} };
+    component.layoutIndex = [];
+    component.dataIndex = [];
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
